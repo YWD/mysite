524a0b50dba726de905bf931fe319362a0192065
杨文冬 <wendong.yang@fuwo.com>
2017-12-07 08:52:08 +0800

    app_name 命名空间

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
 	path('<int:question_id>/', views.detail, name = 'detail'),
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,24 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from .models import Question
+from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
 	#return HttpResponse("Hello World. You are at the polls index.")
-	latest_question_list = Question.objects.order_by('pub_date')[:5]
-	template = loader.get_template('polls/index.html')
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	#template = loader.get_template('polls/index.html')
 	context = {'latest_question_list':latest_question_list,}
-	return HttpResponse(template.render(context))
+	#return HttpResponse(template.render(context))
+	return render(request, 'polls/index.html', context)
 	
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk = question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html',{'question':question})
+	#return HttpResponse("You're looking at question %s." % question_id)
 	
 def results(request, question_id):
 	response = "You're looking at the result of question %s."
